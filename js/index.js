06b45adb6235a5aa8167186b351b71dba535a6b4
Natalie McCroy <nataliemccroy@gmail.com>
2019-05-03 19:44:51 -0500

    grabbed 10 dom elements to target with event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+// 1. Nav bar - Mouseover event
+const navBar = document.querySelector('.nav');
+navBar.addEventListener('mouseover', (event) => { event.target.style.textShadow = '0 0 3px #f3ec19';});
+
+// 2. Logo
+const logo = document.querySelector('.logo-heading');
+
+// 3. 'Welcome to Fun Bus' h2
+const introHeading = document.querySelector('.intro h2');
+
+
+// 4. Intro image
+const introImg = document.querySelector('.intro img');
+
+// 5. Let's go
+const mainHeading = document.querySelector('.text-content h2');
+
+
+// 6. Let's go img
+const mainImg = document.querySelector('.img-content img');
+
+
+// 7. Destination section
+const destSection = document.querySelector('.content-destination');
+
+
+// 8. All the buttons
+const buttons = document.querySelectorAll('.btn');
+
+
+// 9. Destination img
+const contentImg = document.querySelector('.content-destination img');
+
+
+// 10. Footer
+const footer = document.querySelector('.footer');
